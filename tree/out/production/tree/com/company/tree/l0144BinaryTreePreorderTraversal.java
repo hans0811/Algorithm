c1f5041e37cdbbfb4bc02cc2808b9f6ca3073546
Hans <30435194+hans0811@users.noreply.github.com>
2022-07-14 15:07:02 +0800

    Add files via upload

---
+++ tree/out/production/tree/com/company/tree/l0144BinaryTreePreorderTraversal.java
@@ -0,0 +1,20 @@
+package com.hans.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class l0144BinaryTreePreorderTraversal {
+
+    public int maxDepth(TreeNode root) {
+
+        if(root == null) return 0;
+
+        int leftMaxDepth = maxDepth(root.left);
+
+        int rightMaxDepth=maxDepth(root.right);
+
+        return Math.max(leftMaxDepth, rightMaxDepth) + 1;
+
+
+    }
+}
