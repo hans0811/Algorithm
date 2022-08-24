a713d06f7a8d678a5bf767e9c56291457c623157
Hans <30435194+hans0811@users.noreply.github.com>
2022-08-24 13:12:23 +0800

    Add files via upload

---
+++ algorithm/src/power/L00326PowerOfThree/L00326PowerOfThree.go
@@ -0,0 +1,15 @@
+package main
+
+//import "fmt"
+
+func isPowerOfThree(n int) bool {
+    
+    if( n == 0 ) {
+        return false
+    }
+    for (n % 3 == 0){
+        n /= 3
+    }
+    return n == 1
+	
+}
